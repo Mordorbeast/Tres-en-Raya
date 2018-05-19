2c8b4538bf430a63e1863761cc6e8e7615087f2f
Mordorbeast <xavilongogimenez@gmail.com>
2018-05-19 16:51:30 +0200

    Creamos constructor en la clase Tablero

--- 3EnRaya/src/Tablero.java
+++ 3EnRaya/src/Tablero.java
@@ -9,5 +9,22 @@
  * @author SISCU
  */
 public class Tablero {
+    public int turno;
+    public boolean J1Juega, hayFicha;
+    public String pos1,pos2,pos3,pos4,pos5,pos6,pos7,pos8,pos9;
     
+    
+    public Tablero() {
+        this.turno = 1;
+        this.J1Juega = true;
+        this.pos1 = " ";
+        this.pos2 = " ";
+        this.pos3 = " ";
+        this.pos4 = " ";
+        this.pos5 = " ";
+        this.pos6 = " ";
+        this.pos7 = " ";
+        this.pos8 = " ";
+        this.pos9 = " ";
+    }
 }
